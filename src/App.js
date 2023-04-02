6a4bf5ed0885f3a9d9c1b924edd3000c29213ed1
This-is-Ayush-Sharma <thisisayush79@gmail.com>
2023-04-02 23:41:37 +0530

    router fixed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
 import './App.css';
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes
+} from 'react-router-dom';
+
 import NavbarTop from './components/Navbar/Navbar';
 import Lander from './components/Lander/Lander';
-import Footer from './components/basic/Footer/Footer';
+// import Footer from './components/basic/Footer/Footer';
 import Abouts from './components/Abouts/Abouts';
 import Achievement from './components/Achievement/Achievement';
-
 import Contacts from './components/Contact/Contact'
 
 function App() {
   return (
     <>
-      <NavbarTop />
-      <Lander />
-      {/* <Footer /> */}
-      {/* <Abouts /> */}
-      {/* <Achievement /> */}
-      {/* <Contacts /> */}
-    </>  
+
+      <Router>
+        <NavbarTop />
+        <Routes>
+          <Route path='/about' element={<Abouts />} />
+          <Route path='/' element={<Lander />} />
+          <Route path='/contact' element={<Contacts/>} />
+          <Route path='/achievements' element={<Achievement/>} />
+          {/* <Route path='/about' element={<Abouts/>}/>
+        <Route path='/about' element={<Abouts/>}/> */}
+        </Routes>
+      </Router>
+    </>
   );
 }
+
+// {/* <>
+//   <NavbarTop />
+//   <Lander />
+//   {/* <Footer /> */}
+//   {/* <Abouts /> */}
+//   {/* <Achievement /> */}
+//   {/* <Contacts /> */}
+// </> */}
 
 export default App;
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,12 +2,14 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
+import { Link } from 'react-router-dom';
+
 import portfolio from '../asserts/images/portfolio.png'
 function NavbarTop() {
     return (
         <Navbar bg="light" expand="lg" className='border-0 border-bottom border-right border-1 border-warning'>
             <Container>
-                <Navbar.Brand href="#home" style={{fontSize: "1.8rem"}}>
+                <Navbar.Brand href="#home" style={{ fontSize: "1.8rem" }}>
                     <img
                         alt=""
                         src={portfolio}
@@ -19,12 +21,17 @@ function NavbarTop() {
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="ms-auto" style={{ fontSize: "1.2rem" }}>
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#link">Abouts</Nav.Link>
+                    <Nav className="ms-auto" style={{ fontSize: "1.3rem" }}>
+                        <Link className='p-2' to='/'>Home</Link>
+                        <Link className='p-2' to='/projects'>Projects</Link>
+                        <Link className='p-2' to='/skills'>skills</Link>
+                        <Link className='p-2' to='/achievements'>Achievements</Link>
+                        <Link className='p-2' to='/contact'>Contact</Link>
+                        <Link className='p-2' to='/about'>Abouts</Link>
+                        {/* <Nav.Link href="#link">Abouts</Nav.Link>
                         <Nav.Link href="#link">Projects</Nav.Link>
                         <Nav.Link href="#link">Skills</Nav.Link>
-                        <Nav.Link href="#link">Achievements</Nav.Link>
+                        <Nav.Link href="#link">Achievements</Nav.Link> */}
                     </Nav>
                 </Navbar.Collapse>
             </Container>
